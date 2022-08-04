4ff31e69b0127e1a7b1b2567f92a8df664f41e2d
ferraropedro <106950098+ferraropedro@users.noreply.github.com>
2022-08-03 21:58:39 -0300

    main

---
+++ extension.js
@@ -0,0 +1,74 @@
+
+const personas = [];
+const bebidas = [];
+
+class Persona {
+  constructor(nombre, apellido, bebida) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.bebida = bebida;
+  }
+}
+
+class Bebida {
+  constructor(nombre, valor) {
+    this.nombre = nombre;
+    this.valor = valor;
+    this.cantidadAlumnos = 0;
+  }
+}
+
+bebidas.push(new Bebida("fernet", "1500"));
+bebidas.push(new Bebida("vodka", "1500"));
+bebidas.push(new Bebida("champagne", "2000"));
+bebidas.push(new Bebida("vino", "1000"));
+bebidas.push(new Bebida("cocacola", "300"));
+bebidas.push(new Bebida("speed", "200"));
+
+function encotrarBebida(arrayBebidas, bebidaAEncontrar) {
+  for (bebida of arrayBebidas) {
+    if (bebidaAEncontrar == bebida.nombre) return bebida;
+  }
+}
+
+function encontrarIndice(arrayBebidas, bebidaAEncontrar) {
+    for (bebida of arrayBebidas) {
+      if (bebidaAEncontrar == bebida.nombre) return arrayBebidas.indexOf(bebida);
+    }
+}
+
+
+function ingresarPersonas() {
+  let nombre;
+  do {
+    nombre = prompt("Ingrese el nombre de la persona");
+    if (nombre == "") {
+      break;
+    }
+
+    let apellido = prompt("Ingrese el apellido del alumno");
+    let bebida = prompt("Ingrese la bebida del alumno");
+    let persona = new Persona(nombre, apellido, bebida);
+    let bebidaEncontrado = encotrarBebida(bebidas, bebida); //find
+
+    if (bebidaEncontrado) {
+      if (bebidaEncontrado.cantidadAlumnos < 3) {
+        personas.push(persona);
+        let indice = encontrarIndice(bebidas, bebida); //findIndex
+        bebidas[indice].cantidadAlumnos += 1;
+        alert(
+          "el valor de la bebida es " +
+            bebidas[indice].valor +
+            " y la cantidad botellas es " +
+            bebidas[indice].cantidadAlumnos
+        );
+      } else {
+        alert("no quedan mas bebidas");
+      }
+    } else {
+      alert("la bebida no existe");
+    }
+  } while (nombre != "");
+}
+
+ingresarPersonas();
